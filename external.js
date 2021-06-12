f25a00f3f01ba95c588adf142d9c4f78c758ed95
LOVEJOT BRAR <84431502+Lovejotbrar@users.noreply.github.com>
2021-06-12 13:37:32 +0530

    Update external.js

--- external.js
+++ external.js
@@ -1,10 +1,18 @@
 
 function click1(){
   var y= document.getElementById("click1")
-                             
+     var z=document.getElementById("anim1")                        
   var x= document.getElementById("txt1")
   if(x.style.display=="none"){
     x.style.display="block";
+    z.style.animation = 'none';
+
+			
+
+    z.offsetHeight; 
+
+
+    z.style.animation = null; 
     y.style.backgroundColor="orange";}
   else{
     x.style.display="none";
