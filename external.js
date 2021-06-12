70a5caca800150d8c20497fbe1b2d333f8a3311c
LOVEJOT BRAR <84431502+Lovejotbrar@users.noreply.github.com>
2021-06-12 12:48:25 +0530

    Update external.js

--- external.js
+++ external.js
@@ -1,8 +1,12 @@
 
 function click1(){
+  var y= document.getElementById("click1")
+                             
   var x= document.getElementById("txt1")
   if(x.style.display=="none"){
-    x.style.display="block";}
+    x.style.display="block";
+    y.style.backgroundColor="springreen";}
   else{
-    x.style.display="none";}
+    x.style.display="none";
+    y.style.backgroundColor="#303030";}
   }
