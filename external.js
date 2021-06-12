e4f7f69789d6d9876bfc98642725b020ef1d884a
LOVEJOT BRAR <84431502+Lovejotbrar@users.noreply.github.com>
2021-06-12 18:12:35 +0530

    Update external.js

--- external.js
+++ external.js
@@ -3,18 +3,24 @@ function click1(){
   var y= document.getElementById("click1")
      var z=document.getElementById("anim1")                        
   var x= document.getElementById("txt1")
+  var a=document.getElementById("txt2")                        
+
+  var b= document.getElementById("txt3")
   if(x.style.display=="none"){
     x.style.display="block";
     z.style.animation = 'none';
-
+   a.style.display="block";
 			
 
     z.offsetHeight; 
 
-
+    b.style.display="block";
     z.style.animation = null; 
     y.style.backgroundColor="orange";}
   else{
+	  a.style.display="none";
+	  b.style.display="none";
+	
     x.style.display="none";
     y.style.backgroundColor="#303030";}
   }
