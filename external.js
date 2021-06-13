60b2c41af49417ff2a4973387f6c8f188835e009
LOVEJOT BRAR <84431502+Lovejotbrar@users.noreply.github.com>
2021-06-13 09:42:48 +0530

    Update external.js

--- external.js
+++ external.js
@@ -26,10 +26,12 @@ function click1(){
   }
 
 
-function menu(){var y=document.getElementById("menu")
+function menu(){var z=document.getElementById("contact")
+	 y=document.getElementById("menu")
 	
 	 var x=document.getElementById("click1")
-if(x.style.top =="0px"){y.style.innerText="Menu";
+if(x.style.top =="0px"){z.style.top="-50px";
+	y.style.innerText="Menu";
 	y.style.backgroundColor="#303030";
 			
 	
@@ -37,7 +39,8 @@ if(x.style.top =="0px"){y.style.innerText="Menu";
 			
 	       }else
 
-{y.style.innerText="back";
+{z.style.top="0px";
+	y.style.innerText="back";
 	y.style.backgroundColor="red";
 		       x.style.top="0px";}}
 	
