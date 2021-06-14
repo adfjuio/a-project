943af5af4b08924920489f975d45df9cd793a569
LOVEJOT BRAR <84431502+Lovejotbrar@users.noreply.github.com>
2021-06-14 10:54:41 +0530

    Update external.js

--- external.js
+++ external.js
@@ -4,11 +4,16 @@ function contact(){document.getElementById("contact").style.backgroundColor="dod
 	  
 
 function menu(){var z=document.getElementById("contact")
-	 y=document.getElementById("menu1")
+	
+var a =document.getElementById("menu2")
+var y=document.getElementById("menu")
+		
+
+
 	
 	 var x=document.getElementById("click1")
 if(x.style.top =="0px"){z.style.top="-50px";
-	y.style.innerText="Menu";
+	.style.innerText="Menu";
 	y.style.backgroundColor="#303030";
 			
 	
@@ -17,7 +22,7 @@ if(x.style.top =="0px"){z.style.top="-50px";
 	       }else
 
 {z.style.top="0px";
-	y.style.innerText="back";
+	.style.innerText="back";
 	y.style.backgroundColor="red";
 		       x.style.top="0px";}}
 	
