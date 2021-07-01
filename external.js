96c2e4c67e70b76dd5fe11d94369a454d974e612
LOVEJOT BRAR <84431502+Lovejotbrar@users.noreply.github.com>
2021-07-01 16:33:47 +0530

    Update external.js

--- external.js
+++ external.js
@@ -15,50 +15,61 @@ function scrollFunction() {
 }
 
 function click1(){document.getElementById("click1").style.backgroundColor="dodgerblue";}
+
 function contact(){document.getElementById("contact").style.backgroundColor="dodgerblue";}
+
 	
+
 	  
 
 function menu(){var z=document.getElementById("contact")
+
+var b=document.getElementById("register")
+
 	
+
 var a =document.getElementById("menu1")
+
 var y=document.getElementById("menu")
+
 		
 
+	
+
+	 var x=document.getElementById("click1")
+
+if(x.style.top =="0px"){a.src="https://www.linkpicture.com/q/IMG_20210619_070818.jpg";
+
+	a.innerText="Menu";
+
+	z.style.top="-50px";
+
+	b.style.top="-50px";
 
 	
-	 var x=document.getElementById("click1")
-if(x.style.top =="0px"){a.src="https://www.linkpicture.com/q/IMG_20210619_070818.jpg";
-	a.innerText="Menu";
-	z.style.top="-50px";
-	
+
 	y.style.backgroundColor="#303030";
+
 			
+
 	
+
 	       x.style.top="-50px";
+
 			
+
 	       }else
 
 {a.src="https://www.linkpicture.com/q/PicsArt_06-19-08.31.30.jpg";
+
 	a.innerText="Back";
+
 	z.style.top="0px";
-	
+
+	b.style.top="0px";
+
 	y.style.backgroundColor="red";
+
 		       x.style.top="0px";}}
-	
-	
 
-	
-
-	
-
-	
-
-	
-
-	
-
-	
-
-	
 	
