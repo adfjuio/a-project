561a17549023f7ef49e394218d81cdea29499a82
LOVEJOT BRAR <84431502+Lovejotbrar@users.noreply.github.com>
2021-07-05 15:33:40 +0530

    Update external.js

--- external.js
+++ external.js
@@ -22,7 +22,9 @@ function click1(){document.getElementById("click1").style.backgroundColor="dodge
 
 function contact(){document.getElementById("contact").style.backgroundColor="dodgerblue";}
 
-		  
+	
+
+	  
 
 function menu(){var z=document.getElementById("contact")
 
@@ -35,6 +37,10 @@ var a =document.getElementById("menu1")
 var y=document.getElementById("menu")
 
 		
+
+var d =document.getElementById("icn")
+
+var c=document.getElementById("frm")
 
 	
 
@@ -58,6 +64,16 @@ if(x.style.top =="0px"){a.src="https://www.linkpicture.com/q/IMG_20210619_070818
 
 	       x.style.top="-50px";
 
+	c.style.left="270px";
+
+	c.style.width="160px";
+
+	
+
+	d.style.backgroundColor="#07051a";
+
+	d.style.color="white";
+
 			
 
 	       }else
@@ -70,12 +86,489 @@ if(x.style.top =="0px"){a.src="https://www.linkpicture.com/q/IMG_20210619_070818
 
 	b.style.top="0px";
 
+	c.style.left="130px";
+
+	c.style.width="36px";
+
+	d.style.backgroundColor="white";
+
+	d.style.color="#07051a";
+
 	y.style.backgroundColor="red";
 
 		       x.style.top="0px";}}
 
 	
 
+	function srch(){
+
+	document.getElementById("menu1").src="https://www.linkpicture.com/q/IMG_20210619_070818.jpg";
+
+document.getElementById("click1").style.top="-50px";
+
+document.getElementById("contact").style.top="-50px";
+
+document.getElementById("register").style.top="-50px";
+
+document.getElementById("frm").style.width="160px";
+
+document.getElementById("frm").style.left="270px";
+
+document.getElementById("icn").style.color="white";
+
+document.getElementById("icn").style.backgroundColor="#07051a";
+
+	
+
+  
+
+  
+
+var e1 = document.getElementById("s1");
+
+var e2 = document.getElementById("s2");
+
+var e3 = document.getElementById("s3");
+
+var e4 = document.getElementById("s4");
+
+var e5 = document.getElementById("s5");
+
+var e6 = document.getElementById("s6");
+
+var e7 = document.getElementById("s7");
+
+var colors001 = inpt.value;
+
+if (colors001=="Guess the number"){
+
+e1.scrollIntoView();
+
+e1.scrollIntoView(false);
+
+e1.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Guess"){
+
+e1.scrollIntoView();
+
+e1.scrollIntoView(false);
+
+e1.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Number"){
+
+e1.scrollIntoView();
+
+e1.scrollIntoView(false);
+
+e1.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="guess the number"){
+
+e1.scrollIntoView();
+
+e1.scrollIntoView(false);
+
+e1.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="guess"){
+
+e1.scrollIntoView();
+
+e1.scrollIntoView(false);
+
+e1.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="number"){
+
+e1.scrollIntoView();
+
+e1.scrollIntoView(false);
+
+e1.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Car race game"){
+
+e2.scrollIntoView();
+
+e2.scrollIntoView(false);
+
+e2.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Car race"){
+
+e2.scrollIntoView();
+
+e2.scrollIntoView(false);
+
+e2.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Car game"){
+
+e2.scrollIntoView();
+
+e2.scrollIntoView(false);
+
+e2.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Car"){
+
+e2.scrollIntoView();
+
+e2.scrollIntoView(false);
+
+e2.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="car race game"){
+
+e2.scrollIntoView();
+
+e2.scrollIntoView(false);
+
+e2.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="car race"){
+
+e2.scrollIntoView();
+
+e2.scrollIntoView(false);
+
+e2.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="car game"){
+
+e2.scrollIntoView();
+
+e2.scrollIntoView(false);
+
+e2.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="car"){
+
+e2.scrollIntoView();
+
+e2.scrollIntoView(false);
+
+e2.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Neon tiles"){
+
+e3.scrollIntoView();
+
+e3.scrollIntoView(false);
+
+e3.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Tiles"){
+
+e3.scrollIntoView();
+
+e3.scrollIntoView(false);
+
+e3.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="tiles"){
+
+e3.scrollIntoView();
+
+e3.scrollIntoView(false);
+
+e3.scrollIntoView({behavior: "smooth",block: "center"});}
+
+else if (colors001=="neon"){
+
+e3.scrollIntoView();
+
+e3.scrollIntoView(false);
+
+e3.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Neon"){
+
+e3.scrollIntoView();
+
+e3.scrollIntoView(false);
+
+e3.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Doodle game"){
+
+e4.scrollIntoView();
+
+e4.scrollIntoView(false);
+
+e4.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="doodle game"){
+
+e4.scrollIntoView();
+
+e4.scrollIntoView(false);
+
+e4.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="doodle"){
+
+e4.scrollIntoView();
+
+e4.scrollIntoView(false);
+
+e4.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Doodle"){
+
+e4.scrollIntoView();
+
+e4.scrollIntoView(false);
+
+e4.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="doodle jump"){
+
+e4.scrollIntoView();
+
+e4.scrollIntoView(false);
+
+e4.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Doodle jump"){
+
+e4.scrollIntoView();
+
+e4.scrollIntoView(false);
+
+e4.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Tic tac toe"){
+
+e5.scrollIntoView();
+
+e5.scrollIntoView(false);
+
+e5.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="tic tac toe"){
+
+e5.scrollIntoView();
+
+e5.scrollIntoView(false);
+
+e5.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Tic tac"){
+
+e5.scrollIntoView();
+
+e5.scrollIntoView(false);
+
+e5.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="tic tac"){
+
+e5.scrollIntoView();
+
+e5.scrollIntoView(false);
+
+e5.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Flappy bird 2"){
+
+e6.scrollIntoView();
+
+e6.scrollIntoView(false);
+
+e6.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="bird"){
+
+e6.scrollIntoView();
+
+e6.scrollIntoView(false);
+
+e6.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="flappy bird 2"){
+
+e6.scrollIntoView();
+
+e6.scrollIntoView(false);
+
+e6.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="bird"){
+
+e6.scrollIntoView();
+
+e6.scrollIntoView(false);
+
+e6.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Bird"){
+
+e6.scrollIntoView();
+
+e6.scrollIntoView(false);
+
+e6.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="bird game"){
+
+e6.scrollIntoView();
+
+e6.scrollIntoView(false);
+
+e6.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Bird game"){
+
+e6.scrollIntoView();
+
+e6.scrollIntoView(false);
+
+e6.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Match the cards"){
+
+e7.scrollIntoView();
+
+e7.scrollIntoView(false);
+
+e7.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="match the cards"){
+
+e7.scrollIntoView();
+
+e7.scrollIntoView(false);
+
+e7.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="cards"){
+
+e7.scrollIntoView();
+
+e7.scrollIntoView(false);
+
+e7.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Cards"){
+
+e7.scrollIntoView();
+
+e7.scrollIntoView(false);
+
+e7.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="Match"){
+
+e7.scrollIntoView();
+
+e7.scrollIntoView(false);
+
+e7.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if (colors001=="match"){
+
+e7.scrollIntoView();
+
+e7.scrollIntoView(false);
+
+e7.scrollIntoView({behavior: "smooth",block: "center"});
+
+}
+
+else if(colors001==""){
+
+	alert("Atleast fill something, how there can be a result if you have filled nothing");}
+
+else{
+
+	alert("No result \nTry changing the keywords or fill the correct name as the game name searched by you might not be a content of the website.");
+
+	}
+
+}
 	
 
 	
